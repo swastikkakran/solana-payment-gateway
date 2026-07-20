edd4ea728bb00dd5d106a10bf47067812c64f1de
Swastik Kakran <kakranswastik2007@gmail.com>
2026-07-20 21:25:52 +0530

    add encrypt util

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -8,5 +8,24 @@ const generateSecretKey = function () {
     return crypto.randomBytes(32).toString("hex")
 }
 
+const encrypt = function (text) {
+
+    const encryptionKey = process.env.ENCRYPTION_MASTER_KEY;
+    const algorithm = 'aes-256-gcm';
+    const iv = crypto.randomBytes(12)
+    
+    const cipher = crypto.createCipheriv(algorithm, encryptionKey, iv);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    const authTag = cipher.getAuthTag().toString('hex');
+
+    return {
+            iv: iv.toString('hex'),
+            authTag: authTag,
+            ciphertext: encrypted
+        };
+};
+
+
 
 export { generateApiKey, generateSecretKey }
