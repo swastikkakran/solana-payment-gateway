d11c668ec0f5b93a933fa640580b611bb38dd2ad
Swastik Kakran <kakranswastik2007@gmail.com>
2026-07-21 16:13:03 +0530

    add auth router in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+//route imports
+import authRouter from "./routes/auth.route.js";
 
 const app = express()
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(cors())
 
+//auth router
+app.use("/auth", authRouter)
 
 
 app.use((err, req, res, next) => {
