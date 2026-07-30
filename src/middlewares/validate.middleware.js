979c211ec912f621501bf28287e1755003f523b3
Swastik Kakran <kakranswastik2007@gmail.com>
2026-07-30 13:07:34 +0530

    resolve 'map' error in zod validation since zod v4 has .issues, not .errors(v3) while resolving validation errors

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -4,7 +4,7 @@ export const validateRequest = (schema) => (req, res, next) => {
     const result = schema.safeParse(req.body);
     
     if (!result.success) {
-        const errors = result.error.errors.map(e => ({
+        const errors = result.error.issues.map(e => ({
             field: e.path[0],
             message: e.message
         }));
