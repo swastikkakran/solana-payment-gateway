89a6185fdc8c0513124813f4eade6a004b630df5
Swastik Kakran <kakranswastik2007@gmail.com>
2026-08-20 16:44:42 +0530

    update payment.validator.js

--- src/validators/payment.validator.ts
+++ src/validators/payment.validator.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import { ApiError } from "../utils/api-error.js";
+import { Request, Response, NextFunction } from "express";
 
-const validateObjectId = (paramName) => (req, res, next) => {
-    const id = req.params[paramName];
+const validateObjectId = (paramName: string) => (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params[paramName] as string;
     if (!mongoose.Types.ObjectId.isValid(id)) {
         throw new ApiError(400, `Invalid ${paramName}`);
     }
