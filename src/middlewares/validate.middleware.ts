8e78599af7354a8b7f86888c164ff4d668882834
Swastik Kakran <kakranswastik2007@gmail.com>
2026-08-20 17:19:32 +0530

    update validate.middleware.js

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -1,11 +1,13 @@
+import { ZodType } from "zod";
 import { ApiError } from "../utils/api-error.js";
+import { Request, Response, NextFunction } from "express";
 
-export const validateRequest = (schema) => (req, res, next) => {
+export const validateRequest = (schema: ZodType) => (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
     
     if (!result.success) {
         const errors = result.error.issues.map(e => ({
-            field: e.path[0],
+            field: e.path[0] as string | number | undefined,
             message: e.message
         }));
         throw new ApiError(400, "Validation failed", errors);
