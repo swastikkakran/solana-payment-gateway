c9d2d88e80e4aba178a92ed96f4d67f5fd78fa2a
Swastik Kakran <kakranswastik2007@gmail.com>
2026-08-20 18:20:28 +0530

    update db.js

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import { ApiError } from "../utils/api-error.js";
 
 const connectDB = async function () {
 
     try {
+        if (!process.env.MONGO_URI) throw new ApiError(404, "mongo uri not found!")
         await mongoose.connect(process.env.MONGO_URI)
         console.log("DB connected successfully...");
         
